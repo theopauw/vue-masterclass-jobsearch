e65ff13098d0306d0240d53c513ecb1fcc6fb6d6
Theo Pauw <theopauw@sun.ac.za>
2024-01-14 10:52:17 +0200

    added Profile Image component

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,14 @@
+<template>
+  <img :src="imageLink" class="h-8 w-8 rounded-3xl object-contain" />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      imageLink: "public/link.png",
+    };
+  },
+};
+</script>
