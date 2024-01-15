497334c7b30291e6e1955b9bebe270c302a786de
Theo Pauw <theopauw@sun.ac.za>
2024-01-15 11:04:09 +0200

    make button style dynamic based on props

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,13 +1,31 @@
 <template>
-  <button
-    class="rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white hover:shadow-blue"
-  >
-    Sign in
+  <button :class="buttonClass">
+    {{ text }}
   </button>
 </template>
 
 <script>
 export default {
   name: "ActionButton",
+  props: ["text", "type"],
+  computed: {
+    buttonClass() {
+      return { primary: this.type === "primary", secondary: this.type === "secondary" };
+    },
+  },
 };
 </script>
+
+<style scoped>
+button {
+  @apply rounded px-5 py-3 font-medium;
+}
+
+.primary {
+  @apply border-0 bg-brand-blue-1 text-white hover:shadow-blue;
+}
+
+.secondary {
+  @apply hover:bg-brand-blue-2 bg-transparent text-brand-blue-1 hover:text-white;
+}
+</style>
