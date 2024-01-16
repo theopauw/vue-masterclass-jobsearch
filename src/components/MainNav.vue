c6e18986fd85e51eb8b3364833c62eff6ea66d02
Theo Pauw <theopauw@sun.ac.za>
2024-01-16 12:41:58 +0200

    added Subnav with FontAwesome icon

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -15,6 +15,7 @@
           <action-button v-else text="Sign in" @click="loginUser" />
         </div>
       </div>
+      <the-subnav />
     </div>
   </header>
 </template>
@@ -22,12 +23,14 @@
 <script>
 import ActionButton from "@/components/ActionButton.vue";
 import ProfileImage from "@/components/ProfileImage.vue";
+import TheSubnav from "@/components/TheSubnav.vue";
 
 export default {
   name: "MainNav",
   components: {
     ActionButton,
     ProfileImage,
+    TheSubnav,
   },
   data() {
     return {
