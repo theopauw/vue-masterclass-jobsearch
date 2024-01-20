71c0fd8aad9f3cbf46498b61be632e986d016af4
Theo Pauw <theopauw@sun.ac.za>
2024-01-20 16:27:53 +0200

    made hero text change on timer, styled it

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -1,7 +1,11 @@
 <template>
   <section>
-    <h1>Build for everyone</h1>
-    <h2>Find your next job at Bobo Corp</h2>
+    <h1 class="mb-14 text-8xl font-bold tracking-tighter">
+      <span :class="actionClasses">{{ action }}</span>
+      <br />
+      for everyone
+    </h1>
+    <h2 class="text-3xl font-light">Find your next job at Bobo Corp.</h2>
   </section>
 </template>
 
@@ -10,11 +14,51 @@ export default {
   name: "TheHeadline",
   data() {
     return {
-      sample: "Hello!",
+      action: "Build",
+      interval: null,
     };
   },
+  computed: {
+    actionClasses() {
+      return {
+        build: this.action === "Build",
+        create: this.action === "Create",
+        design: this.action === "Design",
+        code: this.action === "Code",
+      };
+    },
+  },
   created() {
-    console.log("hey I have been create", this.sample);
+    this.changeTitle();
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+  },
+  methods: {
+    changeTitle() {
+      this.interval = setInterval(() => {
+        const actions = ["Build", "Create", "Design", "Code"];
+        const currentActionIndex = actions.indexOf(this.action);
+        const nextActionIndex = (currentActionIndex + 1) % 4;
+        const nextAction = actions[nextActionIndex];
+        this.action = nextAction;
+      }, 3000);
+    },
   },
 };
 </script>
+
+<style scoped>
+.build {
+  color: #1a73e8;
+}
+.create {
+  color: #34a853;
+}
+.design {
+  color: #f9ab00;
+}
+.code {
+  color: #d93025;
+}
+</style>
