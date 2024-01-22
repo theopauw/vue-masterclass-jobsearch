e79f6043f82432b819da92d1847d058ec63cfa2e
Theo Pauw <theopauw@sun.ac.za>
2024-01-22 19:09:50 +0200

    styling to improve search button

--- src/components/JobSearchForm.vue
+++ src/components/JobSearchForm.vue
@@ -1,9 +1,36 @@
 <template>
-  <form action="">I will be in a better form eventually</form>
+  <form class="flex h-12 w-full items-center rounded-3xl border border-solid border-brand-gray-3">
+    <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
+    <div class="flex h-full flex-1 flex-nowrap text-base font-light">
+      <div class="relative flex h-full flex-1 items-center pr-3">
+        <label class="absolute -top-10 left-0">Role</label>
+        <input
+          type="text"
+          placeholder="Software Engineer"
+          class="w-full text-lg font-normal focus:outline-none"
+        />
+      </div>
+      <span class="flex items-center h-full border-l border-r border-brand-gray-3 bg-brand-gray-2 px-3">
+        in
+      </span>
+      <div class="relative flex h-full flex-1 items-center pl-3">
+        <label class="absolute -top-10 left-0">Where?</label>
+        <input
+          type="text"
+          placeholder="Los Angeles"
+          class="w-full text-lg font-normal focus:outline-none"
+        />
+      </div>
+    </div>
+    <action-button text="Search" type="secondary" class="rounded-r-3xl"/>
+  </form>
 </template>
 
 <script>
-  export default{
-    name: "JobSearchForm", 
-  }
+import ActionButton from "./ActionButton.vue";
+
+export default {
+  name: "JobSearchForm",
+  components: { ActionButton },
+};
 </script>
