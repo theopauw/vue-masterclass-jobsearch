77bf5d1560e26b369536a383b529e0fab86876b0
Theo Pauw <theopauw@sun.ac.za>
2024-01-27 11:18:37 +0200

    handle textinput events inline in jobsearchform

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -4,7 +4,7 @@
     <div class="flex h-full flex-1 flex-nowrap text-base font-light">
       <div class="relative flex h-full flex-1 items-center pr-3">
         <label class="absolute -top-10 left-0">Role</label>
-        <text-input placeholder="Software engineer" @handle-input="updateRole" />
+        <text-input placeholder="Software engineer" @handle-input="role = $event" />
       </div>
       <span
         class="flex h-full items-center border-l border-r border-brand-gray-3 bg-brand-gray-2 px-3"
@@ -13,7 +13,7 @@
       </span>
       <div class="relative flex h-full flex-1 items-center pl-3">
         <label class="absolute -top-10 left-0">Where?</label>
-        <text-input placeholder="Los Angeles" @handle-input="updateLocation"/>
+        <text-input placeholder="Los Angeles" @handle-input="location = $event"/>
       </div>
     </div>
     <action-button text="Search" type="secondary" class="rounded-r-3xl" />
@@ -32,14 +32,6 @@ export default {
       role: "",
       location: "",
     };
-  },
-  methods: {
-    updateRole(payload) {
-      this.role = payload;
-    },
-    updateLocation(payload) {
-      this.location = payload;
-    },
   },
 };
 </script>
