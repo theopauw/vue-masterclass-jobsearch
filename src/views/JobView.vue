7a57abcb8f2188f3003531b24530bf4fdb471d55
Theo Pauw <theopauw@sun.ac.za>
2024-02-04 11:16:11 +0200

    get current job id from url

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,10 +1,16 @@
 <template>
-  <div>Job Page</div>
+  <div>Job Page for job {{ currentJobId }}</div>
 </template>
 
 <script>
 export default {
   name: "JobView",
+  computed: {
+    currentJobId() {
+      //return dynamic section of url path (which we named id in the route)
+      return this.$route.params.id;
+    }
+  }
 }
 </script>
 
