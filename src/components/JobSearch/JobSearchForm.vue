a7cdcd28501a65ba346cf6784663695363d71859
Theo Pauw <theopauw@sun.ac.za>
2024-02-04 11:17:51 +0200

    add logic to handle job search submission - encode query params into url

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -1,19 +1,20 @@
 <template>
-  <form class="flex h-12 w-full items-center rounded-3xl border border-solid border-brand-gray-3">
+  <form
+    class="flex h-12 w-full items-center rounded-3xl border border-solid border-brand-gray-3"
+    @submit.prevent="searchForJobs"
+  >
     <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
     <div class="flex h-full flex-1 flex-nowrap text-base font-light">
       <div class="relative flex h-full flex-1 items-center pr-3">
         <label class="absolute -top-10 left-0">Role</label>
-        <text-input placeholder="Software engineer" v-model="role" />
+        <text-input v-model="role" placeholder="Software engineer" />
       </div>
-      <span
-        class="flex h-full items-center border-l border-r border-brand-gray-3 bg-brand-gray-2 px-3"
-      >
+      <span class="flex h-full items-center border-x border-brand-gray-3 bg-brand-gray-2 px-3">
         in
       </span>
       <div class="relative flex h-full flex-1 items-center pl-3">
         <label class="absolute -top-10 left-0">Where?</label>
-        <text-input placeholder="Los Angeles" v-model="location" />
+        <text-input v-model="location" placeholder="Los Angeles" />
       </div>
     </div>
     <action-button text="Search" type="secondary" class="rounded-r-3xl" />
@@ -32,6 +33,14 @@ export default {
       role: "",
       location: "",
     };
+  },
+  methods: {
+    searchForJobs() {
+      this.$router.push({
+        name: "JobResults",
+        query: { role: this.role, location: this.location },
+      });
+    },
   },
 };
 </script>
