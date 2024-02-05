4d596507b740cf5b10a71e8fcfec6d3e4c26550d
Theo Pauw <theopauw@sun.ac.za>
2024-02-05 18:17:15 +0200

    added tests for JobSearchForm

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -6,15 +6,15 @@
     <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
     <div class="flex h-full flex-1 flex-nowrap text-base font-light">
       <div class="relative flex h-full flex-1 items-center pr-3">
-        <label class="absolute -top-10 left-0">Role</label>
-        <text-input v-model="role" placeholder="Software engineer" />
+        <label for="role" class="absolute -top-10 left-0">Role</label>
+        <text-input id="role" v-model="role" placeholder="Software engineer" />
       </div>
       <span class="flex h-full items-center border-x border-brand-gray-3 bg-brand-gray-2 px-3">
         in
       </span>
       <div class="relative flex h-full flex-1 items-center pl-3">
-        <label class="absolute -top-10 left-0">Where?</label>
-        <text-input v-model="location" placeholder="Los Angeles" />
+        <label for="location" class="absolute -top-10 left-0">Where?</label>
+        <text-input id="location" v-model="location" placeholder="Los Angeles" />
       </div>
     </div>
     <action-button text="Search" type="secondary" class="rounded-r-3xl" />
