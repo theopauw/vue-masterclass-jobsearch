e070d40c33675c6aa31f0a45e421c9126b7d784a
Theo Pauw <theopauw@sun.ac.za>
2024-02-05 19:15:13 +0200

    added Job Listings component, updated styling

---
+++ src/components/JobResults/JobListings.vue
@@ -0,0 +1,9 @@
+<template>
+  <main class="flex-auto bg-brand-gray-2 p-8">Job Listings Component</main>
+</template>
+
+<script>
+export default {
+  name: "JobListings",
+};
+</script>
--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,16 +1,19 @@
 <template>
-  <div>
+  <div class="flex w-full flex-row flex-nowrap">
     <job-filters-sidebar />
+    <job-listings />
   </div>
 </template>
 
 <script>
 import JobFiltersSidebar from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue";
+import JobListings from "@/components/JobResults/JobListings.vue";
 
 export default {
   name: "JobResultsView",
   components: {
     JobFiltersSidebar,
+    JobListings,
   },
 };
 </script>
