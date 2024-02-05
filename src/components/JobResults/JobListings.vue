3fe9397c18a49c36f3b88d866f64b835de8c7bdd
Theo Pauw <theopauw@sun.ac.za>
2024-02-05 19:41:20 +0200

    added hardcoded Job Listing

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -1,5 +1,42 @@
 <template>
-  <main class="flex-auto bg-brand-gray-2 p-8">Job Listings Component</main>
+  <main class="flex-auto bg-brand-gray-2 p-8">
+    <ol>
+      <li class="mb-7">
+        <router-link
+          to="/jobs/results/1"
+          class="hover:shadow-gray mx-auto block rounded border border-solid border-brand-gray-2 bg-white"
+        >
+          <div class="mx-8 border-b border-solid border-brand-gray-2 pb-2 pt-5">
+            <h2 class="mb-2 text-2xl">Technical Program Manager, Perception, Augmented Reality</h2>
+            <div class="flex flex-row align-middle">
+              <div class="mr-5">
+                <span>Bobo</span>
+              </div>
+              <div>
+                <span>San Francisco, CA, USA</span>
+              </div>
+            </div>
+          </div>
+          <div class="px-8 py-4">
+            <h3 class="mb-2 mt-1">Qualifications</h3>
+            <div>
+              <ul class="list-disc pl-8">
+                <li>Bachelors degree or equivalent practical experience.</li>
+                <li>5 years of experience in program management.</li>
+                <li>
+                  Experience analyzing data through querying database (e.g. SQL) using spreadsheets
+                  and creating statistical models.
+                </li>
+              </ul>
+            </div>
+            <div class="mt-2 text-center">
+              <router-link to="/jobs/results/1" class="text-brand-blue-1">Expand</router-link>
+            </div>
+          </div>
+        </router-link>
+      </li>
+    </ol>
+  </main>
 </template>
 
 <script>
