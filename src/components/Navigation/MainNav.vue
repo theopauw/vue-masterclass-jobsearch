f93783ac6c8c6334abfc1f53d4beea6847f2659b
Theo Pauw <theopauw@sun.ac.za>
2024-02-18 14:30:54 +0200

    change mainnav to store loginstatus in Pinia

--- src/components/Navigation/MainNav.vue
+++ src/components/Navigation/MainNav.vue
@@ -8,21 +8,26 @@
         <nav class="ml-12 h-full">
           <ul class="flex h-full list-none">
             <li v-for="menuItem in menuItems" :key="menuItem.text" class="ml-9 h-full first:ml-0">
-              <router-link :to="menuItem.url" class="flex h-full items-center py-2.5">{{ menuItem.text }}</router-link>
+              <router-link :to="menuItem.url" class="flex h-full items-center py-2.5">{{
+                menuItem.text
+              }}</router-link>
             </li>
           </ul>
         </nav>
         <div class="ml-auto flex h-full items-center">
-          <profile-image v-if="isLoggedIn" />
-          <action-button v-else text="Sign in" @click="loginUser" />
+          <profile-image v-if="userStore.isLoggedIn" />
+          <action-button v-else text="Sign in" @click="userStore.loginUser" />
         </div>
       </div>
-      <the-subnav v-if="isLoggedIn" />
+      <the-subnav v-if="userStore.isLoggedIn" />
     </div>
   </header>
 </template>
 
 <script>
+import { mapStores } from "pinia";
+import { useUserStore } from "@/stores/user";
+
 import ActionButton from "@/components/Shared/ActionButton.vue";
 import ProfileImage from "@/components/Navigation/ProfileImage.vue";
 import TheSubnav from "@/components/Navigation/TheSubnav.vue";
@@ -44,20 +49,15 @@ export default {
         { text: "Students", url: "/" },
         { text: "Jobs", url: "/jobs/results" },
       ],
-      isLoggedIn: false,
     };
   },
   computed: {
+    ...mapStores(useUserStore), //we named store "user", so access it as this.userStore
     headerHeightClass() {
       return {
-        "h-16": !this.isLoggedIn,
-        "h-32": this.isLoggedIn,
+        "h-16": !this.userStore.isLoggedIn,
+        "h-32": this.userStore.isLoggedIn,
       };
-    },
-  },
-  methods: {
-    loginUser() {
-      this.isLoggedIn = true;
     },
   },
 };
