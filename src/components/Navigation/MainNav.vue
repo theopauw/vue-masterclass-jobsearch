69de0a153dca1097bde6dadc9320231825f896d8
Theo Pauw <theopauw@sun.ac.za>
2024-02-20 19:30:00 +0200

    switch from mapStore to mapActions

--- src/components/Navigation/MainNav.vue
+++ src/components/Navigation/MainNav.vue
@@ -15,17 +15,17 @@
           </ul>
         </nav>
         <div class="ml-auto flex h-full items-center">
-          <profile-image v-if="userStore.isLoggedIn" />
-          <action-button v-else text="Sign in" @click="userStore.loginUser" />
+          <profile-image v-if="isLoggedIn" />
+          <action-button v-else text="Sign in" @click="loginUser" />
         </div>
       </div>
-      <the-subnav v-if="userStore.isLoggedIn" />
+      <the-subnav v-if="isLoggedIn" />
     </div>
   </header>
 </template>
 
 <script>
-import { mapStores } from "pinia";
+import { mapActions, mapState } from "pinia";
 import { useUserStore } from "@/stores/user";
 
 import ActionButton from "@/components/Shared/ActionButton.vue";
@@ -52,13 +52,16 @@ export default {
     };
   },
   computed: {
-    ...mapStores(useUserStore), //we named store "user", so access it as this.userStore
+    ...mapState(useUserStore, ["isLoggedIn"]),
     headerHeightClass() {
       return {
-        "h-16": !this.userStore.isLoggedIn,
-        "h-32": this.userStore.isLoggedIn,
+        "h-16": !this.isLoggedIn,
+        "h-32": this.isLoggedIn,
       };
     },
+  },
+  methods: {
+    ...mapActions(useUserStore, ["loginUser"]),
   },
 };
 </script>
