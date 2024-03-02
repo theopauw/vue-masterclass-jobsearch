07c17a48e0b96838a4849690408f7e6e0ab6ab5f
Theo Pauw <theopauw@sun.ac.za>
2024-03-02 08:24:38 +0200

    made header a prop

--- src/components/Shared/CollapsibleAccordion.vue
+++ src/components/Shared/CollapsibleAccordion.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="border-b border-solid border-brand-gray-2 py-5">
-    <div class="flex cursor-pointer flex-wrap items-center justify-between" @click="open">
-      <h3 class="text-base font-semibold">Organisations</h3>
+    <div
+      class="flex cursor-pointer flex-wrap items-center justify-between"
+      role="button"
+      @click="open"
+    >
+      <h3 class="text-base font-semibold">{{ header }}</h3>
       <font-awesome-icon :icon="caretIcon" />
     </div>
   </div>
@@ -13,6 +17,12 @@
 <script>
 export default {
   name: "CollapsibleAccordion",
+  props: {
+    header: {
+      type: String,
+      required: true,
+    },
+  },
   data() {
     return {
       isOpen: false,
