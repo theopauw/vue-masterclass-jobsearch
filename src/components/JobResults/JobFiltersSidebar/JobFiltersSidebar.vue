27ed5c806fb1b3c834036c7914506eb9bec9d9dd
Theo Pauw <theopauw@sun.ac.za>
2024-03-03 13:33:23 +0200

    refactoring

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -8,33 +8,8 @@
         </div>
       </div>
       <collapsible-accordion header="Degree" />
-      <collapsible-accordion header="Job Types">
-        Placeholder for now
-      </collapsible-accordion>
-      <collapsible-accordion header="Organisations">
-        <div class="mt-5">
-          <fieldset>
-            <ul class="flex flex-row flex-wrap">
-              <li class="h-8 w-1/2">
-                <input id="VueTube" type="checkbox" class="mr-3" />
-                <label for="VueTube">VueTube</label>
-              </li>
-              <li class="h-8 w-1/2">
-                <input id="Between Vue" type="checkbox" class="mr-3" />
-                <label for="Between Vue">Between Vue</label>
-              </li>
-              <li class="h-8 w-1/2">
-                <input id="Et Vue Brute" type="checkbox" class="mr-3" />
-                <label for="Et Vue Brute">Et Vue Brute</label>
-              </li>
-              <li class="h-8 w-1/2">
-                <input id="Vue and a Half Men" type="checkbox" class="mr-3" />
-                <label for="Vue and a Half Men">Vue and a Half Men</label>
-              </li>
-            </ul>
-          </fieldset>
-        </div>
-      </collapsible-accordion>
+      <collapsible-accordion header="Job Types"> Placeholder for now </collapsible-accordion>
+      <job-filters-sidebar-organisations />
     </section>
   </div>
 </template>
@@ -42,8 +17,10 @@
 <script>
 import ActionButton from "@/components/Shared/ActionButton.vue";
 import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
+import JobFiltersSidebarOrganisations from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganisations.vue";
+
 export default {
-  components: { ActionButton, CollapsibleAccordion },
+  components: { ActionButton, CollapsibleAccordion, JobFiltersSidebarOrganisations },
   name: "JobFiltersSidebar",
 };
 </script>
--- tests/unit/components/Shared/CollapsibleAccordion.test.js
+++ tests/unit/components/Shared/CollapsibleAccordion.test.js
@@ -5,7 +5,7 @@ import { describe } from "vitest";
 import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
 
 describe("CollapsibleAccordion", () => {
-  it("renders child content", async () => {
+  const renderCollapsibleAccordion = (config = {}) => {
     render(CollapsibleAccordion, {
       global: {
         stubs: {
@@ -18,7 +18,19 @@ describe("CollapsibleAccordion", () => {
       slots: {
         default: "<h3>My nested child</h3>",
       },
+      ...config,
     });
+  };
+  it("renders child content", async () => {
+    const props = {
+      header: "My Category",
+    };
+    const slots = {
+      default: "<h3>My nested child</h3>",
+    };
+    const config = { props, slots };
+    renderCollapsibleAccordion(config);
+    
     expect(screen.queryByText("My nested child")).not.toBeInTheDocument();
     const button = screen.getByRole("button", { name: /my category/i });
     await userEvent.click(button);
@@ -27,20 +39,16 @@ describe("CollapsibleAccordion", () => {
 
   describe("when parent does not provide custom child content", () => {
     it("renders default content", async () => {
-      render(CollapsibleAccordion, {
-        global: {
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-        props: {
-          header: "My Category",
-        },
-      });
+      const props = {
+        header: "My Category",
+      };
+      const slots = {};
+      const config = { props, slots };
+      renderCollapsibleAccordion(config);
       
       const button = screen.getByRole("button", { name: /my category/i });
       await userEvent.click(button);
       expect(screen.getByText("Whoops, somebody forgot to populate me")).toBeInTheDocument();
-    })
-  })
+    });
+  });
 });
