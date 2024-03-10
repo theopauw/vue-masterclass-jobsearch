2cfef127254cf23d1cf883b9aadb2f8d84290d71
Theo Pauw <theopauw@sun.ac.za>
2024-03-10 16:14:44 +0200

    added Teams View

--- src/components/Navigation/MainNav.vue
+++ src/components/Navigation/MainNav.vue
@@ -42,7 +42,7 @@ export default {
   data() {
     return {
       menuItems: [
-        { text: "Teams", url: "/" },
+        { text: "Teams", url: "/teams" },
         { text: "Locations", url: "/" },
         { text: "Life at Bobo Corp", url: "/" },
         { text: "How we hire", url: "/" },
