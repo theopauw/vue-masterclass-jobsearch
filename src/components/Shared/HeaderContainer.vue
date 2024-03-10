822c89ca3d9a41ecdd8665b8f621831b0d3b2537
Theo Pauw <theopauw@sun.ac.za>
2024-03-10 14:31:44 +0200

    add HeaderContainer for title/subtitle

---
+++ src/components/Shared/HeaderContainer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="border-brand-gray1 w-full border-b border-solid"></div>
+  <div class="mx-auto my-16 text-center">
+    <slot name="title">Sample title</slot>
+    <slot name="subtitle">Sample subtitle</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderContainer",
+};
+</script>
