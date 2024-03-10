3c452412c71363ea8561c186efe9f27d5702d67d
Theo Pauw <theopauw@sun.ac.za>
2024-03-10 16:13:37 +0200

    typo

--- src/components/Shared/HeaderContainer.vue
+++ src/components/Shared/HeaderContainer.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="border-brand-gray1 w-full border-b border-solid"></div>
-  <div class="mx-auto my-16 text-center">
-    <slot name="title">Sample title</slot>
-    <slot name="subtitle">Sample subtitle</slot>
+  <div class="border-brand-gray1 w-full border-b border-solid">
+    <div class="mx-auto my-16 text-center">
+      <slot name="title">Sample title</slot>
+      <slot name="subtitle">Sample subtitle</slot>
+    </div>
   </div>
 </template>
 
