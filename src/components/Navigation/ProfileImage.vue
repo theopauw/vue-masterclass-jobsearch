a4084e97b4904b0608a3dc444baa2976d047251c
Theo Pauw <theopauw@sun.ac.za>
2024-04-07 14:54:43 +0200

    convert ProfileImage component to ts

--- src/components/Navigation/ProfileImage.vue
+++ src/components/Navigation/ProfileImage.vue
@@ -2,13 +2,8 @@
   <img :src="imageLink" class="h-8 w-8 rounded-3xl object-contain" alt="User profile image" />
 </template>
 
-<script>
-export default {
-  name: "ProfileImage",
-  data() {
-    return {
-      imageLink: "public/link.png",
-    };
-  },
-};
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const imageLink = ref("public/link.png");
 </script>
