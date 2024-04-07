1aa20ac47027598207616c5a86909efa13c2b93e
Theo Pauw <theopauw@sun.ac.za>
2024-04-07 13:40:14 +0200

    refactor jobs test with Jobs factory function

--- tests/unit/stores/jobs.test.ts
+++ tests/unit/stores/jobs.test.ts
@@ -38,6 +38,20 @@ describe("actions", () => {
 });
 
 describe("getters", () => {
+  const createJob = (job: Partial<Job> = {}): Job => ({
+    id: 1,
+    title: "Angular Developer",
+    organization: "Vue and Me",
+    degree: "Master's",
+    jobType: "Intern",
+    locations: ["Lisbon"],
+    minimumQualifications: ["Mesh granular deliverables"],
+    preferredQualifications: ["Mesh wireless metrics"],
+    description: ["Away someone forget effect wait land."],
+    dateAdded: "2021-07-04",
+    ...job,
+  });
+
   beforeEach(() => {
     setActivePinia(createPinia());
   });
@@ -46,10 +60,10 @@ describe("getters", () => {
     it("finds unique organisations from list of jobs", () => {
       const store = useJobsStore();
       store.jobs = [
-        { organization: "Google" },
-        { organization: "Amazon" },
-        { organization: "Google" },
-      ] as Job[];
+        createJob({ organization: "Google" }),
+        createJob({ organization: "Amazon" }),
+        createJob({ organization: "Google" }),
+      ];
 
       const result = store.UNIQUE_ORGANISATIONS;
 
@@ -61,10 +75,10 @@ describe("getters", () => {
     it("finds unique job types from list of jobs", () => {
       const store = useJobsStore();
       store.jobs = [
-        { jobType: "Full-time" },
-        { jobType: "Temporary" },
-        { jobType: "Full-time" },
-      ] as Job[];
+        createJob({ jobType: "Full-time" }),
+        createJob({ jobType: "Temporary" }),
+        createJob({ jobType: "Full-time" }),
+      ];
 
       const result = store.UNIQUE_JOB_TYPES;
 
@@ -78,7 +92,7 @@ describe("getters", () => {
         const userStore = useUserStore();
         userStore.selectedOrganisations = [];
         const jobsStore = useJobsStore();
-        const job = { organization: "Google" } as Job;
+        const job = createJob({ organization: "Google" });
 
         const result = jobsStore.INCLUDE_JOB_BY_ORGANISATION(job);
 
@@ -90,7 +104,7 @@ describe("getters", () => {
       const userStore = useUserStore();
       userStore.selectedOrganisations = ["Google", "Microsoft"];
       const jobsStore = useJobsStore();
-      const job = { organization: "Google" } as Job;
+      const job = createJob({ organization: "Google" });
 
       const result = jobsStore.INCLUDE_JOB_BY_ORGANISATION(job);
 
@@ -104,7 +118,7 @@ describe("getters", () => {
         const userStore = useUserStore();
         userStore.selectedJobTypes = [];
         const jobsStore = useJobsStore();
-        const job = { jobType: "Full-time" } as Job;
+        const job = createJob({ jobType: "Full-time" }) as Job;
 
         const result = jobsStore.INCLUDE_JOB_BY_JOB_TYPE(job);
 
@@ -116,7 +130,7 @@ describe("getters", () => {
       const userStore = useUserStore();
       userStore.selectedJobTypes = ["Full-time", "Part-time"];
       const jobsStore = useJobsStore();
-      const job = { jobType: "Full-time" } as Job;
+      const job = createJob({ jobType: "Full-time" });
 
       const result = jobsStore.INCLUDE_JOB_BY_JOB_TYPE(job);
 
