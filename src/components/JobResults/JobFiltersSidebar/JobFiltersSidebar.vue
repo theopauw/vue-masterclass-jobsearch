c9cd5c491e66311baec9ffbe3bc165de6aa8f291
Theo Pauw <theopauw@sun.ac.za>
2024-04-11 20:08:40 +0200

    split up JobFiltersSidebar into sub components

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -11,43 +11,29 @@
           ></action-button>
         </div>
       </div>
+
       <collapsible-accordion header="Degrees">
-        <job-filters-sidebar-checkbox-group
-          :unique-values="UNIQUE_DEGREES"
-          :action="userStore.ADD_SELECTED_DEGREES"
-      /></collapsible-accordion>
+        <job-filters-sidebar-degrees />
+      </collapsible-accordion>
 
       <collapsible-accordion header="Job Types">
-        <job-filters-sidebar-checkbox-group
-          :unique-values="UNIQUE_JOB_TYPES"
-          :action="userStore.ADD_SELECTED_JOB_TYPES"
-      /></collapsible-accordion>
+        <job-filters-sidebar-job-types />
+      </collapsible-accordion>
+
       <collapsible-accordion header="Organisations">
-        <job-filters-sidebar-checkbox-group
-          :unique-values="UNIQUE_ORGANISATIONS"
-          :action="userStore.ADD_SELECTED_ORGANISATIONS"
-      /></collapsible-accordion>
+        <job-filters-sidebar-organisations />
+      </collapsible-accordion>
     </section>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed } from "vue";
-
 import ActionButton from "@/components/Shared/ActionButton.vue";
 import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
-import JobFiltersSidebarCheckboxGroup from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarCheckboxGroup.vue";
-
-import { useJobsStore } from "@/stores/jobs";
+import JobFiltersSidebarDegrees from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarDegrees.vue";
+import JobFiltersSidebarJobTypes from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarJobTypes.vue";
+import JobFiltersSidebarOrganisations from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganisations.vue";
 import { useUserStore } from "@/stores/user";
-import { useDegreesStore } from "@/stores/degrees";
-
-const jobsStore = useJobsStore();
-const UNIQUE_ORGANISATIONS = computed(() => jobsStore.UNIQUE_ORGANISATIONS);
-const UNIQUE_JOB_TYPES = computed(() => jobsStore.UNIQUE_JOB_TYPES);
-
-const degreesStore = useDegreesStore();
-const UNIQUE_DEGREES = computed(() => degreesStore.UNIQUE_DEGREES);
 
 const userStore = useUserStore();
 </script>
---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganisations.vue
@@ -0,0 +1,18 @@
+<template>
+  <job-filters-sidebar-checkbox-group
+    :unique-values="UNIQUE_ORGANISATIONS"
+    :action="userStore.ADD_SELECTED_ORGANISATIONS"
+  />
+</template>
+<script lang="ts" setup>
+import { computed } from "vue";
+
+import JobFiltersSidebarCheckboxGroup from "./JobFiltersSidebarCheckboxGroup.vue";
+import { useJobsStore } from "@/stores/jobs";
+import { useUserStore } from "@/stores/user";
+
+const jobsStore = useJobsStore();
+const UNIQUE_ORGANISATIONS = computed(() => jobsStore.UNIQUE_ORGANISATIONS);
+
+const userStore = useUserStore();
+</script>
