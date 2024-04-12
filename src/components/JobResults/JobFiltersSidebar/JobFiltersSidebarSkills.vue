fe730a2ac041bb629c5f798295a77f958471925a
Theo Pauw <theopauw@sun.ac.za>
2024-04-12 19:21:13 +0200

    add input modifiers to better deal with input

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarSkills.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mt-2">
     <input
-      v-model="skillsSearchTerm"
+      v-model.lazy.trim="skillsSearchTerm"
       type="text"
       class="h-12 w-full rounded border border-solid border-brand-gray-1 text-base shadow-gray"
       placeholder="Computer programming, Finance degree"
